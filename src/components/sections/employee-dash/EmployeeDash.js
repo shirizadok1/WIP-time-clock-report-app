e4c90294c2227a0a4a25c6d1c522665b103d58db
שמעון בוזגלו <shirizadok1@gmail.com>
2023-02-11 17:29:16 +0200

    monthly report api

--- src/components/sections/employee-dash/EmployeeDash.js
+++ src/components/sections/employee-dash/EmployeeDash.js
@@ -1,40 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const EmployeeDash = () => {
+const EmployeeDashboard = () => {
   const [startTime, setStartTime] = useState(null);
-  const [endTime, setEndTime] = useState(null);
+  const [stopTime, setStopTime] = useState(null);
+  const [monthlyReport, setMonthlyReport] = useState([]);
+  const [showReport, setShowReport] = useState(false);
 
-  const handleStart = () => {
+  const startClock = () => {
     setStartTime(new Date());
   };
 
-  const handleStop = () => {
-    setEndTime(new Date());
-
-    // Add logic to save the working hours to the database
+  const stopClock = () => {
+    setStopTime(new Date());
   };
+
+  useEffect(() => {
+    const fetchMonthlyReport = async () => {
+      try {
+        const response = await axios.get('/api/monthly_report');
+        setMonthlyReport(...monthlyReport,response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchMonthlyReport();
+  }, [monthlyReport]);
+
+ 
 
   return (
     <div>
-      <button onClick={handleStart} disabled={startTime}>
-        Start
-      </button>
-      <button onClick={handleStop} disabled={!startTime || endTime}>
-        Stop
-      </button>
+      <h1>Employee Dashboard</h1>
+      <button onClick={startClock}>Start Clock</button>
+      <button onClick={stopClock}>Stop Clock</button>
       {startTime && (
         <div>
           <p>Start Time: {startTime.toString()}</p>
         </div>
       )}
-      {endTime && (
+      {stopTime && (
         <div>
-          <p>End Time: {endTime.toString()}</p>
+          <p>Stop Time: {stopTime.toString()}</p>
         </div>
       )}
-      <button>View Monthly Report</button>
+
+      <button onClick={() => setShowReport(!showReport)}>
+        View Monthly Report
+      </button>
+
+      {showReport && (
+        <div>
+          <h2>Monthly Report</h2>
+          <ul>
+            {monthlyReport.map((report) => (
+              <li key={report.id}>
+                {report.date}: {report.startTime} - {report.stopTime}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
 
-export default EmployeeDash;
+export default EmployeeDashboard;
